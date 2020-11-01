394b6f65cb436a70cefeb2ab8a29b2498b67e23b
ScotSche <se721kal@htwg-konstanz.de>
2020-11-01 16:16:15 +0100

    Implement BoardSpec tests

--- src/main/scala/model/Board.scala
+++ src/main/scala/model/Board.scala
@@ -3,6 +3,7 @@ package model
 case class Board(stones: BoardMatrix[Stone]) {
   def this(size: Int) = this(new BoardMatrix[Stone](size, Stone(0)))
 
+  def stone(rect_num: Int, pos_num: Int) = stones.stone(rect_num, pos_num)
 
   def update_board(rectangle_num: Int, position_num:Int, color:Int) : Board ={
     copy(stones.replaceStone(rectangle_num, position_num, Stone(color)))
--- src/test/scala/model/BoardSpec.scala
+++ src/test/scala/model/BoardSpec.scala
@@ -4,4 +4,40 @@ import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
 
 class BoardSpec extends AnyWordSpec with Matchers{
+  "A board" when {
+    "created properly but empty" should {
+      val board = new Board(24)
+      "give access to its Stones" in {
+        board.stone(0, 0) should be(Stone(0))
+        board.stone(0, 1) should be(Stone(0))
+        board.stone(0, 2) should be(Stone(0))
+        board.stone(0, 3) should be(Stone(0))
+        board.stone(0, 4) should be(Stone(0))
+        board.stone(0, 5) should be(Stone(0))
+        board.stone(0, 6) should be(Stone(0))
+        board.stone(0, 7) should be(Stone(0))
+        board.stone(1, 0) should be(Stone(0))
+        board.stone(1, 1) should be(Stone(0))
+        board.stone(1, 2) should be(Stone(0))
+        board.stone(1, 3) should be(Stone(0))
+        board.stone(1, 4) should be(Stone(0))
+        board.stone(1, 5) should be(Stone(0))
+        board.stone(1, 6) should be(Stone(0))
+        board.stone(1, 7) should be(Stone(0))
+        board.stone(2, 0) should be(Stone(0))
+        board.stone(2, 1) should be(Stone(0))
+        board.stone(2, 2) should be(Stone(0))
+        board.stone(2, 3) should be(Stone(0))
+        board.stone(2, 4) should be(Stone(0))
+        board.stone(2, 5) should be(Stone(0))
+        board.stone(2, 6) should be(Stone(0))
+        board.stone(2, 7) should be(Stone(0))
+      }
+      "allow to set individual Stones and remain immutable" in {
+        val changedBoard = board.update_board(0, 0, 1)
+        changedBoard.stone(0, 0) should be(Stone(1))
+        board.stone(0, 0) should be(Stone(0))
+      }
+    }
+  }
 }
